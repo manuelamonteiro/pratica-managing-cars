020e5b2dc087c6debfcf9c0715a16abfc8913fa1
manuelamp <manuella.m.p182@gmail.com>
2023-03-07 13:39:13 -0300

    refactor: update message error

--- src/services/carService.ts
+++ src/services/carService.ts
@@ -38,7 +38,7 @@ type CarUpdate = {
 }
 
 async function updateCar(id: number, car: CarUpdate) {
-  if ({ car }) throw { name: "empty_payload_error", message: "não foi especificada nenhuma mudança" };
+  if ({ car }) throw { name: "empty_payload_error", message: "No changes specified." };
   await carRepository.updateCar(id, car)
 
 }
